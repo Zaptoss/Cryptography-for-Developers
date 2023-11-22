7daabb00b42f86a30cf2450a966d31ec6f8fbb41
Zaptos <57064743+Zaptoss@users.noreply.github.com>
2023-11-22 13:39:23 +0000

    Upload wrapper for crypto/elliptic library
    Used curve: P224
    Some of the functions used are deprecated, but there are two libraries for implementing the digital signature and the Diffie-Hellman protocol on elliptic curves, which clearly do not provide all the functions for working with elliptic curves.

---
+++ EllipticCurveWrapper/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	wrapper "ElipticCurveWrapper/wrapper"
+	"crypto/rand"
+	"fmt"
+)
+
+func main() {
+
+	G := wrapper.BasePointGGet()
+	k, _ := rand.Prime(rand.Reader, 256)
+	d, _ := rand.Prime(rand.Reader, 256)
+
+	H1 := wrapper.ScalarMult(d, G)
+	H2 := wrapper.ScalarMult(k, H1)
+
+	H3 := wrapper.ScalarMult(k, G)
+	H4 := wrapper.ScalarMult(d, H3)
+	fmt.Println(H2)
+	fmt.Println(H4)
+	if H2.X.String() == H4.X.String() && H2.Y.String() == H4.Y.String() {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
+
+}
