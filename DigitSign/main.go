f819552aeb1dbf5c66096a449e7b7bf07959e3c8
Zaptos <57064743+Zaptoss@users.noreply.github.com>
2023-11-29 23:25:19 +0200

    Update main.go

--- DigitSign/main.go
+++ DigitSign/main.go
@@ -4,53 +4,46 @@ import (
 	"crypto/rand"
 	"fmt"
 	"math/big"
+	"DigitSign/sha1"
 )
 
 var keyLength = 2048
 var bigOne = big.NewInt(1)
 var bigNegOne = big.NewInt(-1)
 
-func KeyGen() (pv, pb *big.Int) {
-	var p, q *big.Int
-	// var p1 = big.NewInt(0)
-	// var q1 = big.NewInt(0)
-	// var n = big.NewInt(0)
-	var Fn = big.NewInt(0)
-	var e = big.NewInt(0xfffe)
-	var d = big.NewInt(1)
-	p, _ = rand.Prime(rand.Reader, keyLength/2)
-	q, _ = rand.Prime(rand.Reader, keyLength/2)
+func KeyGen() (pb, pv, n *big.Int) {
+	var p, q, Fn, d *big.Int
+	var e = big.NewInt(0xffff)
 
-	// n = new(big.Int).Mul(p, q)
-	// p1.Sub(p, bigOne)
-	// q1.Sub(q, bigOne)
+	for true {
+		p, _ = rand.Prime(rand.Reader, keyLength/2)
+		q, _ = rand.Prime(rand.Reader, keyLength/2)
+		Fn = new(big.Int).Mul(new(big.Int).Sub(p, bigOne), new(big.Int).Sub(q, bigOne))
 
-	// fmt.Println(e)
-	Fn = new(big.Int).Mul(new(big.Int).Sub(p, bigOne), new(big.Int).Sub(q, bigOne))
+		if new(big.Int).GCD(nil, nil, e, Fn).String() == "1" {
+			break
+		}
+	}
 
-	fmt.Println(Fn)
 	d = new(big.Int).ModInverse(e, Fn)
-	fmt.Println(d)
+	n = new(big.Int).Mul(p, q)
 
-	// fmt.Println(e, d)
-	// e.Mul(e, d)
-	// fmt.Println(e)
-	// e.Mod(e, n)
-	// fmt.Println(e)
+	return e, d, n
+}
 
-	return e, d
+func Sign(fileName string, pv, n *big.Int) (r *big.Int) {
+	return new(big.Int).Exp(fileName, pv, n)
+}
+
+func Verify(fileName string, signature, pb, n *big.Int) (bool) {
+
 }
 
 func main() {
-	KeyGen()
-	// a := big.NewInt(20)
-	// b := big.NewInt(30)
-	// a.Mul(a, b)
-	// fmt.Println(a)
-
-	// n := big.NewInt(0xffff)
-	// mod, _ := rand.Prime(rand.Reader, keyLength/2)
-	// inv := new(big.Int).ModInverse(n, mod)
-	// fmt.Println(inv)
+	pb, pv, n := KeyGen()
+	fmt.Printf("Public Key: %s\nPrivate Key: %s\nn: %s\n", pb.String(), pv.String(), n.String())
+	enc := EncryptDecrypt(big.NewInt(10), pb, n)
+	dec := EncryptDecrypt(enc, pv, n)
+	fmt.Println(dec)
 
 }
