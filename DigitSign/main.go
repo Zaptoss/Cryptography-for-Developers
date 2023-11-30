331e1fccf0cb618d4e8d69853c8520d01c56dc20
Zaptos <57064743+Zaptoss@users.noreply.github.com>
2023-11-30 09:31:52 +0200

    Add files via upload

--- DigitSign/main.go
+++ DigitSign/main.go
@@ -1,74 +1,119 @@
 package main
 
 import (
-	sha1 "DigitSign/sha1"
-	"crypto/rand"
+	rsa "DigitSign/rsa"
+	"bufio"
 	"fmt"
 	"math/big"
+	"os"
 )
 
-var keyLength = 2048
-var bigOne = big.NewInt(1)
-var bigNegOne = big.NewInt(-1)
-
-func KeyGen() (pb, pv, n *big.Int) {
-	var p, q, Fn, e, d *big.Int
-
-	for true {
-		e, _ = rand.Prime(rand.Reader, keyLength)
-		p, _ = rand.Prime(rand.Reader, keyLength/2)
-		q, _ = rand.Prime(rand.Reader, keyLength/2)
-		Fn = new(big.Int).Mul(new(big.Int).Sub(p, bigOne), new(big.Int).Sub(q, bigOne))
-
-		if new(big.Int).GCD(nil, nil, e, Fn).String() == "1" && e.Cmp(Fn) == -1 {
-			break
-		}
-	}
-
-	n = new(big.Int).Mul(p, q)
-	d = new(big.Int).ModInverse(e, Fn)
-
-	return e, d, n
-}
-
-func SignFile(filePath string, pv, n *big.Int) (s *big.Int) {
-	hash := sha1.NewHash([]byte{})
-	hash.SHA1File(filePath)
-	hashSum, _ := new(big.Int).SetString(hash.GetHex(), 16)
-	return new(big.Int).Exp(hashSum, pv, n)
-}
-
-func VerifyFile(filePath string, signature, pb, n *big.Int) bool {
-	hash := sha1.NewHash([]byte{})
-	hash.SHA1File(filePath)
-	announcedHash := new(big.Int).Exp(signature, pb, n)
-	if announcedHash.Text(16) == hash.GetHex() {
-		return true
-	}
-	return false
-}
-
-func SignMessage(msg string, pv, n *big.Int) (s *big.Int) {
-	hash := sha1.NewHash([]byte(msg))
-	hash.SHA1()
-	hashSum, _ := new(big.Int).SetString(hash.GetHex(), 16)
-	return new(big.Int).Exp(hashSum, pv, n)
-}
-
-func VerifyMessage(msg string, signature, pb, n *big.Int) bool {
-	hash := sha1.NewHash([]byte(msg))
-	hash.SHA1()
-	announcedHash := new(big.Int).Exp(signature, pb, n)
-	if announcedHash.Text(16) == hash.GetHex() {
-		return true
-	}
-	return false
-}
-
 func main() {
-	pb, pv, n := KeyGen()
-	// fmt.Printf("Public Key: %s\nPrivate Key: %s\nn: %s\n", pb.Text(16), pv.Text(16), n.Text(16))
-	enc := SignMessage("test.txt", pv, n)
-	dec := VerifyMessage("test.txt", enc, pb, n)
-	fmt.Println(dec)
+	reader := bufio.NewReader(os.Stdin)
+	// bytes, _, _ := reader.ReadLine()
+	// fmt.Printf(string(bytes))
+	// fmt.Scanln()
+
+	var bytes []byte
+	var state = true
+	for state {
+		fmt.Printf("Enter you command(h - help, q - exit): ")
+		bytes, _, _ = reader.ReadLine()
+		switch string(bytes) {
+		case "h":
+			fmt.Println("## kg - key generation; this command generate p and q, one of this can used as private key and other as public key, and n - modul")
+			fmt.Println("## sf - sign file; it take your private key, n as hex and file path as string for sign file")
+			fmt.Println("## vf - verify file; it take signature, private key and n as hex and file path as string for verify file")
+			fmt.Println("## sm - sign message; it take your private key, n as hex and message as string for sign file")
+			fmt.Println("## vf - verify message; it take signature, private key and n as hex and file path as string for verify message")
+		case "q":
+			state = false
+		case "kg":
+			fmt.Println("It can take some time...")
+			pb, pv, n := rsa.KeyGen()
+			fmt.Printf("-- Public Key: %s\n-- Private Key: %s\n-- n: %s\n", pb.Text(16), pv.Text(16), n.Text(16))
+		case "sf":
+			var filePath, pvS, nS string
+			fmt.Printf("-- Enter filepath: ")
+			bytes, _, _ = reader.ReadLine()
+			filePath = string(bytes)
+			fmt.Printf("-- Enter private key: ")
+			fmt.Scanln(&pvS)
+			fmt.Printf("-- Enter n: ")
+			fmt.Scanln(&nS)
+
+			pv := big.NewInt(0)
+			n := big.NewInt(0)
+
+			pv.SetString(pvS, 16)
+			n.SetString(nS, 16)
+			signature := rsa.SignFile(filePath, pv, n)
+			fmt.Printf("-- Signature is: %s\n", signature.Text(16))
+
+		case "sm":
+			var message, pvS, nS string
+			fmt.Printf("-- Enter message: ")
+			bytes, _, _ = reader.ReadLine()
+			message = string(bytes)
+			fmt.Printf("-- Enter private key: ")
+			fmt.Scanln(&pvS)
+			fmt.Printf("-- Enter n: ")
+			fmt.Scanln(&nS)
+
+			pv := big.NewInt(0)
+			n := big.NewInt(0)
+
+			pv.SetString(pvS, 16)
+			n.SetString(nS, 16)
+			signature := rsa.SignMessage(message, pv, n)
+			fmt.Printf("-- Signature is: %s\n", signature.Text(16))
+		case "vf":
+			var filePath, signatureS, pbS, nS string
+			fmt.Printf("-- Enter filepath: ")
+			fmt.Scanln(&filePath)
+			fmt.Printf("-- Enter signature: ")
+			fmt.Scanln(&signatureS)
+			fmt.Printf("-- Enter public key: ")
+			fmt.Scanln(&pbS)
+			fmt.Printf("-- Enter n: ")
+			fmt.Scanln(&nS)
+
+			signature := big.NewInt(0)
+			pb := big.NewInt(0)
+			n := big.NewInt(0)
+			signature.SetString(signatureS, 16)
+			pb.SetString(pbS, 16)
+			n.SetString(nS, 16)
+			if rsa.VerifyFile(filePath, signature, pb, n) {
+				fmt.Println("Sign verified!")
+			} else {
+				fmt.Println("Sign was corrupted!")
+			}
+		case "vm":
+			var message, signatureS, pbS, nS string
+			fmt.Printf("-- Enter message: ")
+			bytes, _, _ = reader.ReadLine()
+			message = string(bytes)
+			fmt.Printf("-- Enter signature: ")
+			fmt.Scanln(&signatureS)
+			fmt.Printf("-- Enter public key: ")
+			fmt.Scanln(&pbS)
+			fmt.Printf("-- Enter n: ")
+			fmt.Scanln(&nS)
+
+			signature := big.NewInt(0)
+			pb := big.NewInt(0)
+			n := big.NewInt(0)
+
+			signature.SetString(signatureS, 16)
+			pb.SetString(pbS, 16)
+			n.SetString(nS, 16)
+			if rsa.VerifyMessage(message, signature, pb, n) {
+				fmt.Println("Sign verified!")
+			} else {
+				fmt.Println("Sign was corrupted!")
+			}
+		}
+
+	}
 }
---
+++ DigitSign/rsa/rsa.go
@@ -0,0 +1,65 @@
+package DigitSign
+
+import (
+	sha1 "DigitSign/sha1"
+	"crypto/rand"
+	"math/big"
+)
+
+var keyLength = 2048
+var bigOne = big.NewInt(1)
+var bigNegOne = big.NewInt(-1)
+
+func KeyGen() (pb, pv, n *big.Int) {
+	var p, q, Fn, e, d *big.Int
+
+	for true {
+		e, _ = rand.Prime(rand.Reader, keyLength)
+		p, _ = rand.Prime(rand.Reader, keyLength/2)
+		q, _ = rand.Prime(rand.Reader, keyLength/2)
+		Fn = new(big.Int).Mul(new(big.Int).Sub(p, bigOne), new(big.Int).Sub(q, bigOne))
+
+		if new(big.Int).GCD(nil, nil, e, Fn).String() == "1" && e.Cmp(Fn) == -1 {
+			break
+		}
+	}
+
+	n = new(big.Int).Mul(p, q)
+	d = new(big.Int).ModInverse(e, Fn)
+
+	return e, d, n
+}
+
+func SignFile(filePath string, pv, n *big.Int) (s *big.Int) {
+	hash := sha1.NewHash([]byte{})
+	hash.SHA1File(filePath)
+	hashSum, _ := new(big.Int).SetString(hash.GetHex(), 16)
+	return new(big.Int).Exp(hashSum, pv, n)
+}
+
+func VerifyFile(filePath string, signature, pb, n *big.Int) bool {
+	hash := sha1.NewHash([]byte{})
+	hash.SHA1File(filePath)
+	announcedHash := new(big.Int).Exp(signature, pb, n)
+	if announcedHash.Text(16) == hash.GetHex() {
+		return true
+	}
+	return false
+}
+
+func SignMessage(msg string, pv, n *big.Int) (s *big.Int) {
+	hash := sha1.NewHash([]byte(msg))
+	hash.SHA1()
+	hashSum, _ := new(big.Int).SetString(hash.GetHex(), 16)
+	return new(big.Int).Exp(hashSum, pv, n)
+}
+
+func VerifyMessage(msg string, signature, pb, n *big.Int) bool {
+	hash := sha1.NewHash([]byte(msg))
+	hash.SHA1()
+	announcedHash := new(big.Int).Exp(signature, pb, n)
+	if announcedHash.Text(16) == hash.GetHex() {
+		return true
+	}
+	return false
+}
